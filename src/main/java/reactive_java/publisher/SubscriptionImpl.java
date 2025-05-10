cd074d2468aae83b222478c7a8df6613ecbe374c
marwhals <marjan.mubarok2012@gmail.com>
2025-05-10 15:47:09 +0100

    Implement publisher and subscriber

---
+++ src/main/java/reactive_java/publisher/SubscriptionImpl.java
@@ -0,0 +1,45 @@
+package reactive_java.publisher;
+
+import com.github.javafaker.Faker;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubscriptionImpl implements Subscription {
+
+    private static final Logger log = LoggerFactory.getLogger(SubscriptionImpl.class);
+    private static final int MAX_ITEMS = 10;
+    private final Faker faker;
+    private final Subscriber<? super String> subscriber;
+    private boolean isCancelled = false;
+    private int count = 0;
+
+    public SubscriptionImpl(Subscriber<? super String> subscriber) {
+        this.subscriber = subscriber;
+        this.faker = new Faker();
+    }
+
+    @Override
+    public void request(long requested) {
+        if (isCancelled) {
+            return;
+        }
+        log.info("subscriber has requested {} items", requested);
+        for (int i = 0; i < requested && count < MAX_ITEMS; i++) {
+            count++;
+            subscriber.onNext(this.faker.internet().emailAddress());
+        }
+        if(count == MAX_ITEMS){
+            log.info("no more data to produce");
+            this.subscriber.onComplete();
+            this.isCancelled = true;
+        }
+    }
+
+    @Override
+    public void cancel() {
+        log.info("subscriber has cancelled");
+        this.isCancelled = true;
+    }
+}
